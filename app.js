f5514764a319aefed1dfb4a1195e89c889e8f4ba
roza <rozalizadeh@gmail.com>
2023-10-15 15:36:53 +0330

    crud-delete

--- app.js
+++ app.js
@@ -1,59 +1,138 @@
 const Add_btn=document.querySelector("#add-btn");
 const Todo_input=document.querySelector("#todo-input");
 const Todo_list=document.querySelector(".list");
-const real_list=JSON.parse(localStorage.getItem("my_list"));
-for(let i =0 ; i<real_list.length ; i++){
-    const title=real_list[i];
-    addTolist(title);
-}
+const del_btn=document.querySelector("#delete");
 
-Add_btn.addEventListener("click", ()=>{
-    const item=Todo_input.value;
-    validationCheck(item);
-    
-    
-})
-function clearInput(){
-    Todo_input.value=""
+let list=[];
 
-}
-function syncStorage(item){
-    real_list.push(item)
-    const string_List= JSON.stringify(real_list);
-    localStorage.setItem("my_list", string_List);
+function validationCheck(){
+    const val=Todo_input.value;
 
-}
-function validationCheck(item){
-    if(item===""){
+    if(val === ""){
         alert("you can not add empty string!");
     }else{ 
-       syncStorage(item);
-       addTolist(item);
+        const item={
+        title: val,
+        status: false,
+        };
+       addItem(item);
+       renderItem(item);
        clearInput();
 
     }
 }
-function addTolist(item){
+//work with dom
+function renderItem(item){
 
-const div_item=document.createElement("div");
-div_item.classList.add("item");
+    const div_item=document.createElement("div");
+    div_item.classList.add("item");
+    
+    const checkbox=document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.checked=item.status;
+    
+    const span=document.createElement("span");
+    span.textContent=item.title;
+    
+    div_item.appendChild(checkbox);
+    div_item.appendChild(span);
+    Todo_list.appendChild(div_item);
+    checkbox.addEventListener("click", ()=> {
+    toggleStatus(item.title);
+        
+    })
 
-const checkbox=document.createElement("input");
-checkbox.setAttribute("type", "checkbox");
-
-const span=document.createElement("span");
-span.textContent=item;
-
-div_item.appendChild(checkbox);
-div_item.appendChild(span);
-Todo_list.appendChild(div_item);
 
 
 
-
-
-// console.log(list)
-// const p=document.createElement("p")
-// p.textContent=item
-// console.log(p)
+    
 }
+function clearInput(){
+    Todo_input.value=""
+
+}
+function renderList(){
+    for(let i =0 ; i<list.length ; i++){
+        const item=list[i];
+        renderItem(item);
+    }
+
+}
+function remove(){
+    
+    for(let i=0;i<list.length;i++){
+        if(list[i].status===true){
+         list.splice(i,1);
+        }
+
+
+
+        
+     }
+     syncStorage();
+     
+    
+     console.log(list);
+    }
+ 
+
+    
+
+//work with storage
+function syncStorage(){
+      
+    const string_List= JSON.stringify(list);
+    localStorage.setItem("my_list", string_List);
+    console.log(list);
+}
+function loadFromStorage(){
+
+    const listFromStorage=JSON.parse(localStorage.getItem("my_list")) || [];
+   
+    list=listFromStorage;
+   
+}
+function addItem(item){
+
+    const next_item={
+        title: item.title,
+        status: item.status,
+    };
+    list.push(next_item);
+    syncStorage();
+           
+
+}
+//functionality
+function toggleStatus(title){
+    for(let i=0 ; i<list.length ; i++){
+
+        if (list[i].title===title){
+            list[i].status=!list[i].status;
+            
+
+                     }
+    }
+    syncStorage();
+}
+
+
+function events(){
+    Add_btn.addEventListener("click", validationCheck);     
+    }
+    del_btn.addEventListener("click",()=>{
+     remove();
+
+
+
+    })
+   
+
+
+function init(){
+    loadFromStorage();
+    renderList();
+    events();
+    console.log(list);
+}
+init();
